e393d02124ca1c113adc79112c18f3286169e778
andgreendigital <agreen@and.digital>
2021-12-02 10:47:55 +0000

    :sparkles: Day 1 part 1

--- src/day1.js
+++ src/day1.js
@@ -1,9 +1,29 @@
+function compareReduce(array, callback, initialValue) {
+  return array.reduce((accumulator, currentValue, currentIndex) => {
+    if (currentIndex === 0) return accumulator;
+    return callback(accumulator, array[currentIndex - 1], currentValue, currentIndex, array);
+  }, initialValue);
+}
+
+function splitNumericalInput(input) {
+  return input
+    .split('\n')
+    .filter((entry) => entry)
+    .map(Number);
+}
+
 function partOne(input) {
-
+  const sonarReadings = splitNumericalInput(input);
+  return compareReduce(
+    sonarReadings,
+    (total, previousReading, currentReading) => {
+      if (currentReading > previousReading) return total + 1;
+      return total;
+    },
+    0,
+  );
 }
 
-function partTwo(input) {
-
-}
+function partTwo(input) {}
 
 module.exports = { partOne, partTwo };
--- src/day1.test.js
+++ src/day1.test.js
@@ -1,13 +1,20 @@
-const { partOne, partTwo } = require('./day1');
+const { partOne, partTwo } = require("./day1");
 
-const testData = `
+const testData = `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`;
 
-`;
-
-test('Part One', () => {
-  expect(partOne(testData)).toBe();
+test("Part One", () => {
+  expect(partOne(testData)).toBe(7);
 });
 
-test('Part Two', () => {
+test("Part Two", () => {
   expect(partTwo(testData)).toBe();
 });
