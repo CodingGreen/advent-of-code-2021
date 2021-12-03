5e3cd043919fb14bfb9a6157567953ce5705e1d2
andgreendigital <agreen@and.digital>
2021-12-03 07:48:04 +0000

    :sparkles: Day 2 Part 1

--- src/day2.js
+++ src/day2.js
@@ -1,5 +1,38 @@
-function partOne(input) {
+function splitInput(input) {
+  return input
+    .split('\n')
+    .filter((entry) => entry);
+}
 
+function partOne(input) {
+  const rawInstructions = splitInput(input);
+  const instructions = rawInstructions.map((rawInstruction) => {
+    const [command, distance] = rawInstruction.split(' ');
+    return { command, distance: Number(distance) };
+  });
+
+  const commands = {
+    forward: (currentPosition, distance) => ({
+      ...currentPosition,
+      horizontal: currentPosition.horizontal + distance,
+    }),
+    down: (currentPosition, distance) => ({
+      ...currentPosition,
+      depth: currentPosition.depth + distance,
+    }),
+
+    up: (currentPosition, distance) => ({
+      ...currentPosition,
+      depth: currentPosition.depth - distance,
+    }),
+  };
+
+  const position = instructions.reduce(
+    (currentPosition, { command, distance }) => commands[command](currentPosition, distance),
+    { horizontal: 0, depth: 0 },
+  );
+
+  return position.depth * position.horizontal;
 }
 
 function partTwo(input) {
--- src/day2.test.js
+++ src/day2.test.js
@@ -1,11 +1,14 @@
 const { partOne, partTwo } = require('./day2');
 
-const testData = `
-
-`;
+const testData = `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`;
 
 test('Part One', () => {
-  expect(partOne(testData)).toBe();
+  expect(partOne(testData)).toBe(150);
 });
 
 test('Part Two', () => {
