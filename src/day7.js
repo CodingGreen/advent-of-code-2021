424d5430696716e7f94f0562a043c71d9e1c643c
andgreendigital <agreen@and.digital>
2021-12-14 18:27:14 +0000

    :sparkles: Day 7 Part 2

--- src/day7.js
+++ src/day7.js
@@ -8,7 +8,7 @@ function sort(array) {
   return [...array].sort((a, b) => a - b);
 }
 
-function findMedian(sortedArray) {
+function median(sortedArray) {
   return sortedArray[Math.floor(sortedArray.length / 2)];
 }
 
@@ -16,17 +16,43 @@ function difference(a, b) {
   return Math.abs(a - b);
 }
 
+function average(array) {
+  return sum(array) / array.length;
+}
+
+function fuelCost(distance) {
+  return (distance * (distance + 1)) / 2;
+}
+
+function calculateFuelRequirement(positionList, comparisonPosition, calculateFuelCost) {
+  return sum(positionList
+    .map((currentPosition) => calculateFuelCost(difference(currentPosition, comparisonPosition))));
+}
+
+function identityFunction(input) {
+  return input;
+}
+
 function partOne(input) {
   const positionList = splitInput(input);
   const sortedPositions = sort(positionList);
-  const medianPosition = findMedian(sortedPositions);
-  const fuelRequirements = positionList
-    .map((currentPosition) => difference(currentPosition, medianPosition));
-  return sum(fuelRequirements);
+  const medianPosition = median(sortedPositions);
+  const fuelRequirement = calculateFuelRequirement(positionList, medianPosition, identityFunction);
+  return fuelRequirement;
 }
 
 function partTwo(input) {
-
+  const positionList = splitInput(input);
+  const averagePosition = average(positionList);
+  const averagePositionFloor = Math.floor(averagePosition);
+  const averagePositionCeiling = Math.ceil(averagePosition);
+  const averagePositionFloorFuelRequirement = calculateFuelRequirement(
+    positionList, averagePositionFloor, fuelCost,
+  );
+  const averagePositionCeilingFuelRequirement = calculateFuelRequirement(
+    positionList, averagePositionCeiling, fuelCost,
+  );
+  return Math.min(averagePositionFloorFuelRequirement, averagePositionCeilingFuelRequirement);
 }
 
 module.exports = { partOne, partTwo };
