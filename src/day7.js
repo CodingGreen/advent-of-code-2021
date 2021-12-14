bee780ad38fad7cd7e569231d921aeaa8b2cc74f
andgreendigital <agreen@and.digital>
2021-12-14 17:48:18 +0000

    :sparkles: Day 7 Part 1

--- src/day7.js
+++ src/day7.js
@@ -1,5 +1,28 @@
-function partOne(input) {
+const { sum } = require('./utils');
 
+function splitInput(input) {
+  return input.split(',').map(Number);
+}
+
+function sort(array) {
+  return [...array].sort((a, b) => a - b);
+}
+
+function findMedian(sortedArray) {
+  return sortedArray[Math.floor(sortedArray.length / 2)];
+}
+
+function difference(a, b) {
+  return Math.abs(a - b);
+}
+
+function partOne(input) {
+  const positionList = splitInput(input);
+  const sortedPositions = sort(positionList);
+  const medianPosition = findMedian(sortedPositions);
+  const fuelRequirements = positionList
+    .map((currentPosition) => difference(currentPosition, medianPosition));
+  return sum(fuelRequirements);
 }
 
 function partTwo(input) {
--- src/day7.test.js
+++ src/day7.test.js
@@ -1,11 +1,10 @@
 const { partOne, partTwo } = require('./day7');
 
-const testData = `
-
+const testData = `16,1,2,0,4,2,7,1,2,14
 `;
 
 test('Part One', () => {
-  expect(partOne(testData)).toBe();
+  expect(partOne(testData)).toBe(37);
 });
 
 test('Part Two', () => {
