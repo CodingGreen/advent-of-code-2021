ef5820e493dba218e5ef31ea756f15ad2f2029c2
CodingGreen <17253156+CodingGreen@users.noreply.github.com>
2021-12-30 22:24:11 +0000

    :sparkles: Day 12 Part 1

--- src/day12.js
+++ src/day12.js
@@ -1,5 +1,43 @@
-function partOne(input) {
+const { identityFunction, sum } = require('./utils');
 
+function createEdges(input) {
+  return input.split('\n').filter(identityFunction).map((edgeString) => edgeString.split('-'));
+}
+
+function isLowerCase(string) {
+  return string.toUpperCase() !== string;
+}
+
+function findChildrenForEdges(edges) {
+  return (node) => edges
+    .map((edge) => edge.filter((edgeNode) => edgeNode !== node))
+    .filter((edge) => edge.length === 1)
+    .map((array) => array[0]);
+}
+
+function searchGraph(edges) {
+  const findChildren = findChildrenForEdges(edges);
+  const searchGraphRecursive = (pathSoFar, nextCave) => {
+    if (nextCave === 'end') return 1;
+
+    if (isLowerCase(nextCave) && pathSoFar.includes(nextCave)) {
+      return 0;
+    }
+
+    const children = findChildren(nextCave);
+
+    return sum(children.map((child) => searchGraphRecursive(
+      [...pathSoFar, nextCave],
+      child,
+    )));
+  };
+
+  return searchGraphRecursive([], 'start');
+}
+
+function partOne(input) {
+  const edges = createEdges(input);
+  return searchGraph(edges);
 }
 
 function partTwo(input) {
--- src/day12.test.js
+++ src/day12.test.js
@@ -1,13 +1,58 @@
 const { partOne, partTwo } = require('./day12');
 
-const testData = `
-
+test('Part One - Example 1', () => {
+  const testData = `start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
 `;
 
-test('Part One', () => {
-  expect(partOne(testData)).toBe();
+  expect(partOne(testData)).toBe(10);
+});
+
+test('Part One - Example 2', () => {
+  const testData = `dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc
+`;
+
+  expect(partOne(testData)).toBe(19);
+});
+
+test('Part One - Example 3', () => {
+  const testData = `fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
+`;
+
+  expect(partOne(testData)).toBe(226);
 });
 
 test('Part Two', () => {
-  expect(partTwo(testData)).toBe();
+  expect(partTwo()).toBe();
 });
